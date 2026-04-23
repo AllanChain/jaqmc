9a0e08b8423672d6ce479c7e436604d021966bec
钱昱冰 <qyb@bytedance.com>
2026-04-23 20:47:19 +0800

    use RTD URLs in README and metadata

--- docs/conf.py
+++ docs/conf.py
@@ -71,7 +71,7 @@ html_js_files = [
     ("readthedocs.js", {"defer": "defer"}),
 ]
 html_logo = "_static/jaqmc-light-large.svg"
-html_baseurl = "https://bytedance.github.io/jaqmc/"
+html_baseurl = "https://jaqmc.readthedocs.io/latest/"
 html_theme_options = {
     "repository_url": "https://github.com/bytedance/jaqmc",
     "use_source_button": True,
